55a9cb12fd96c9df6f63ee71b764eb5baf71f560
Eduardo Iriarte <eddiriarte@gmail.com>
2018-06-26 21:29:06 +0200

    install docker/docker-compose into box :whale::whale:

--- provision.sh
+++ provision.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ########################
-# Java
+# Install java
 ########################
 echo "Installing Java"
 sudo apt-get -y install openjdk-8-jdk > /dev/null 2>&1
 
 ########################
-# Jenkins
+# Install jenkins
 ########################
 echo "Installing Jenkins"
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
@@ -16,20 +16,36 @@ sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y install jenkins > /dev/null 2>&1
 
 ########################
-# nginx
+# Install nginx
 ########################
 echo "Installing nginx"
 sudo apt-get -y install nginx > /dev/null 2>&1
 sudo service nginx start
-
-########################
-# Configuring nginx
-########################
-echo "Configuring nginx"
 cd /etc/nginx/sites-available
 sudo rm default ../sites-enabled/default
 sudo cp /vagrant/VirtualHost/jenkins /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
 sudo service nginx restart
 sudo service jenkins restart
+
+########################
+# Install docker
+########################
+echo "Installing docker"
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker vagrant
+sudo apt-get install -y docker-compose
+
+########################
+# Done
+########################
+echo "Initial admin password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 echo "Success"
